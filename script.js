3e72cbf512e446babae68486ce93d0860d660731
npr74mjc9k-rgb <npr74mjc9k@privaterelay.appleid.com>
2026-07-16 06:50:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ const navMenu = document.getElementById("nav-menu");
 
 menuBtn.addEventListener("click", () => {
     navMenu.classList.toggle("show");
+
+    if (navMenu.classList.contains("show")) {
+        menuBtn.textContent = "✕";
+    } else {
+        menuBtn.textContent = "☰";
+    }
+});
+
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("show");
+        menuBtn.textContent = "☰";
+    });
 });
